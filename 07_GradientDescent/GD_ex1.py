f1e0575d32dcbb628a98857b063bd80e71ecbecc
tiena2cva <tiena2cva@gmail.com>
2017-10-20 23:02:24 +0700

    plot gif

--- 07_GradientDescent/GD_ex1.py
+++ 07_GradientDescent/GD_ex1.py
@@ -34,15 +34,33 @@ fig.set_tight_layout(True)
 X = np.arange(-5, 5, 0.001)
 
 ax.plot(X, cost(X), 'b')
+ax.set_xlabel('timestep')
 
-cur_point = ax.plot([-5], cost(-5), 'ko')
-new_point = ax.plot([-5], cost(-5), 'ro')
+line, = ax.plot([-5, -5], [cost(-5), cost(-5)], 'k')
+pre_point, = ax.plot(-5, cost(-5), 'ko')
+new_point, = ax.plot(-5, cost(-5), 'ro')
 
 
 def update(i):
     label = 'timestep {0}'.format(i)
     ax.set_xlabel(label)
 
+    if i == 0:
+        pre_data = x1[i]
+    else:
+        pre_data = x1[i - 1]
+    new_data = x1[i]
 
-anim = FuncAnimation(fig, update, frames=np.arange(0, 10), interval=500)
+    pre_point.set_xdata(pre_data)
+    pre_point.set_ydata(cost(pre_data))
+
+    new_point.set_xdata(new_data)
+    new_point.set_ydata(cost(new_data))
+
+    line.set_xdata([pre_data, new_data])
+    line.set_ydata([cost(pre_data), cost(new_data)])
+
+
+anim = FuncAnimation(fig, update, frames=np.arange(0, it1), interval=500)
+anim.save('GD_ex1.gif', dpi=80, writer='imagemagick')
 plt.show()
